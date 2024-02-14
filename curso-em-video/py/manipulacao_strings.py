15e3b20069f415fc21f5a626b5995159e188776d
brunolf23 <138618870+brunolf23@users.noreply.github.com>
2024-02-14 20:02:50 -0300

    aula 10

---
+++ curso-em-video/py/manipulacao_strings.py
@@ -0,0 +1,13 @@
+frase = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.'
+
+print (frase.replace('sed', 'Bruno Lindo'))
+print (len(frase))
+print (frase.count('o'))
+print (frase.lower().find('ipsum'))
+print (frase.split())
+print (frase.capitalize())
+print (frase.title())
+print ('-'.join(frase))
+
+dividido = frase.split()
+print (dividido[2] [2])
