64450a1b595eb40e8cb745c62307b8731434e1b0
brunolf23 <138618870+brunolf23@users.noreply.github.com>
2024-02-14 18:48:02 -0300

    aula 9 exe 3

---
+++ curso-em-video/exercicios/exe026.py
@@ -0,0 +1,5 @@
+letra = str(input('Digite uma frase: ')).upper().strip()
+
+print (f'A letra A aparece {letra.count('A')} vezes na frase.')
+print (f'A primeira letra A apareceu na posição {letra.find('A')+1}')
+print (f'A última letra A apareceu na posição {letra.rfind('A')+1}')
