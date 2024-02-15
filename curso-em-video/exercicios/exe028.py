0996a5e7569f122eb9c70e514b17e68f27166154
brunolf23 <138618870+brunolf23@users.noreply.github.com>
2024-02-15 00:37:07 -0300

    aula 10 exe 1

---
+++ curso-em-video/exercicios/exe028.py
@@ -0,0 +1,17 @@
+from random import randint
+from time import sleep
+
+print ('-=-' * 20)
+print ('Vou pensar em um número de 0 a 5. Tenten adivinhar...')
+print ('-=-' * 20)
+
+numero = int(input('Em que número eu pensei? '))
+lista = randint(0, 5)
+
+print ('Processando...')
+sleep(2)
+
+if numero == lista:
+    print ('Parabéns, você acertou!')
+else:
+    print (f'Errou! O número que eu pensei foi o {lista}.')
---
+++ curso-em-video/exercicios/exe029.py
@@ -0,0 +1,9 @@
+velocidade = int(input('Qual a velocidade do carro? '))
+
+multa = (velocidade - 80) * 7
+
+if velocidade <= 80:
+    print (f'Parabéns, você está a {velocidade}Km/h, limite de velocidade permitido.')
+else:
+    print (f'Você estava a {velocidade}Km/h, por isso foi multado.')
+    print (f'Você tera que pagar {multa}')
