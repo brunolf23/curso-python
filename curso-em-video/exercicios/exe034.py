31bbbb8ab02e6c3ce599c72e7f62b88a109b0512
brunolf23 <138618870+brunolf23@users.noreply.github.com>
2024-02-15 22:15:01 -0300

    aula 10 exe 2

---
+++ curso-em-video/exercicios/exe034.py
@@ -0,0 +1,9 @@
+salario = int(input('Qual o valor do seu salário? R$'))
+
+if salario > 1250:
+    aumento = salario + (salario * 10/100)
+    print (f'Como você recebe {salario}, tera um adicional de 10%. Indo para R${aumento:.2f}')
+    
+else:
+    aumento = salario + (salario * 15/100)
+    print (f'Como você recebe {salario}, tera um adicional de 15%. Indo para R${aumento:.2f}')
