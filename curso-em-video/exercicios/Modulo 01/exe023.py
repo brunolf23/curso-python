345a66e9a3df8430055b7e95457c9589c626a6f7
brunolf23 <138618870+brunolf23@users.noreply.github.com>
2024-02-22 01:21:19 -0300

    exe 45

--- curso-em-video/exercicios/Modulo 01/exe023.py
+++ curso-em-video/exercicios/Modulo 01/exe023.py
@@ -1,10 +1,14 @@
 # Separando dígitos de um número
 
 numero = int(input('Informe um número: '))
-n = str(numero)
 
-print (f'Analisando o número {numero}')
-print (f'Unidade: {n[3]}')
-print (f'Dezena: {n[2]}')
-print (f'Centena: {n[1]}')
-print (f'Milhar: {n[0]}')
+milhar = numero // 1000
+centena = (numero % 1000) // 100
+dezena = (numero % 100) // 10
+unidade = numero % 10
+
+print(f'Analisando o número {numero}')
+print(f'Milhar: {milhar}')
+print(f'Centena: {centena}')
+print(f'Dezena: {dezena}')
+print(f'Unidade: {unidade}')
--- curso-em-video/exercicios/Modulo 02/exe045.py
+++ curso-em-video/exercicios/Modulo 02/exe045.py
@@ -1,15 +1,49 @@
 # Pedra, Papel e Tesoura
 
-from random import choice
+from random import randint
+from time import sleep
+
+lista = ('Pedra', 'Papel', 'Tesoura')
+sortear = randint(0, 2)
 
 print ('''Suas opções:
-       [1] Pedra
-       [2] Papel
-       [3] Tesoura''')
+       [0] Pedra
+       [1] Papel
+       [2] Tesoura''')
 
 jogada = int(input('Qual é a sua jogada? '))
 
-lista = [1, 2, 3]
-sortear = choice(lista)
+print ('-=' * 15)
+print (f'Computador jogou {lista[sortear]}')
+print (f'Jogador jogou {lista[jogada]}')
+print ('-=' * 15)
 
-if jogada == 1:
+if sortear == 0:
+    if jogada == 0:
+        print ('EMPATE')
+        
+    elif jogada == 1:
+        print ('JOGADOR VENCE')
+        
+    elif jogada == 2:
+        print ('COMPUTADOR VENCE')
+        
+elif sortear == 1:
+    if jogada == 0:
+        print ('COMPUTADOR VENCE')
+        
+    elif jogada == 1:
+        print ('EMPATE')
+        
+    elif jogada == 2:
+        print ('JOGADOR VENCE')
+    
+elif sortear == 2:
+    if jogada == 0:
+        print ('JOGADOR VENCE')
+        
+    elif jogada == 1:
+        print ('COMPUTADOR VENCE')
+        
+    elif jogada == 2:
+        print ('EMPATE')
