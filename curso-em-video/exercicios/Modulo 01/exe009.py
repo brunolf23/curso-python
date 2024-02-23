34061ea44fd4f568227d85c1a56c5113b9d29d79
brunolf23 <138618870+brunolf23@users.noreply.github.com>
2024-02-22 21:57:58 -0300

    exe 48 e 49

--- curso-em-video/exercicios/Modulo 01/exe009.py
+++ curso-em-video/exercicios/Modulo 01/exe009.py
@@ -1,26 +1,16 @@
 # Tabuada
 
-inteiro = int(input('Qual número você gostaria de multiplicar?: '))
-#n1 = inteiro * 1
-n2 = inteiro * 2
-n3 = inteiro * 3
-n4 = inteiro * 4
-n5 = inteiro * 5
-n6 = inteiro * 6
-n7 = inteiro * 7
-n8 = inteiro * 8
-n9 = inteiro * 9
-n10 = inteiro * 10
+numero = int(input('Qual número você gostaria de multiplicar?: '))
 
 print('=' * 14)
-print(f'{inteiro} x {1:2} = {inteiro * 1:2}')
-print(f'{inteiro} x {2:2} = {n2}')
-print(f'{inteiro} x {3:2} = {n3}')
-print(f'{inteiro} x {4:2} = {n4}')
-print(f'{inteiro} x {5:2} = {n5}')
-print(f'{inteiro} x {6:2} = {n6}')
-print(f'{inteiro} x {7:2} = {n7}')
-print(f'{inteiro} x {8:2} = {n8}')
-print(f'{inteiro} x {9:2} = {n9}')
-print(f'{inteiro} x {10} = {n10}')
+print(f'{numero} x {1:2} = {numero * 1:2}')
+print(f'{numero} x {2:2} = {numero * 2:2}')
+print(f'{numero} x {3:2} = {numero * 3:2}')
+print(f'{numero} x {4:2} = {numero * 4:2}')
+print(f'{numero} x {5:2} = {numero * 5:2}')
+print(f'{numero} x {6:2} = {numero * 6:2}')
+print(f'{numero} x {7:2} = {numero * 7:2}')
+print(f'{numero} x {8:2} = {numero * 8:2}')
+print(f'{numero} x {9:2} = {numero * 9:2}')
+print(f'{numero} x {10} = {numero * 10:2}')
 print('=' * 14)
---
+++ curso-em-video/exercicios/Modulo 02/exe049.py
@@ -0,0 +1,5 @@
+# Tabuada v.2.0
+
+escolha = int(input('Escolha um número: '))
+for numero in range (1, 11):
+    print (f'{escolha} x {numero:2} = {escolha * numero:2}')
