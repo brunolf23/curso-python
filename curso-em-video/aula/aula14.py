0d267386fd076a7f651e16c71df19ac58f6b38f2
brunolf23 <138618870+brunolf23@users.noreply.github.com>
2024-02-27 00:02:07 -0300

    aula 14

---
+++ curso-em-video/aula/aula14.py
@@ -0,0 +1,18 @@
+# Estruturas de repetição while
+
+'''for i in range (1, 10):
+    print (i)
+print ('FIM')
+
+c = 1
+while c < 10:
+    print (c)
+    c += 1
+print ('FIM')'''
+
+# ---------------------------------------
+
+n = 1 # apenas para ter um início no meu while
+while n != 0:
+    n = int(input('Digite um valor: '))
+print ('FIM')
