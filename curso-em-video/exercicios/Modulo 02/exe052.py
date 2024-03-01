b63eaa88033caab00c77b0359cc193aeca132dd9
brunolf23 <138618870+brunolf23@users.noreply.github.com>
2024-03-01 17:19:47 -0300

    exe 52 e 53

--- curso-em-video/exercicios/Modulo 02/exe052.py
+++ curso-em-video/exercicios/Modulo 02/exe052.py
@@ -1,10 +1,13 @@
-#
+# Números primos
 
 numero = int(input('Digite um número: '))
-
+contador = 0
 for i in range (1, numero + 1):
     if numero % i == 0:
-        print ('\033[34m', end=' ')
-    else:
         print ('\033[33m', end=' ')
+        contador +=1
+    else:
+        print ('\033[32m', end=' ')
     print (f'{i}', end=' ')
+    
+print (f'\n O número {numero} foi divisível {contador} vezez.')
