3eed708ebbdfa11040fad772607cc5d7cc003fdb
brunolf23 <138618870+brunolf23@users.noreply.github.com>
2024-03-07 00:56:55 -0300

    exe 51

--- curso-em-video/exercicios/Modulo 02/exe051.py
+++ curso-em-video/exercicios/Modulo 02/exe051.py
@@ -1,9 +1,9 @@
 # Progressão Aritmética
 
-termo = int(input('Digite o primeiro termo: '))
-razao = int(input('Digite a razão'))
-n = termo + (10 - 1) * razao
+termo = int(input("Digite o primeiro termo: "))
+razao = int(input("Digite a razão: "))
+decimo = termo + (10 - 1) * razao
 
-for i in range (termo, n + 1, razao):
-    print (f'{i}', end=" => ")
-print ('ACABOU')
+for c in range (termo, decimo + razao, razao):
+    print (f"{c}", end=" -> ")
+print ("Acabou!")
