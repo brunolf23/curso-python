1b13058611967f16ee8b119edd212069fb675ef3
brunolf23 <138618870+brunolf23@users.noreply.github.com>
2024-03-08 17:59:03 -0300

    exe 61 e 62

--- curso-em-video/modulo 2/exe061.py
+++ curso-em-video/modulo 2/exe061.py
@@ -1,6 +1,14 @@
 # Progressão Aritmética v.2.0
 
+print ('Gerador de PA')
+print ('-=' * 10)
 termo = int(input('Digite o primeiro termo: '))
-razao = int(input('Digite a razão'))
+razao = int(input('Digite a razão: '))
+termo = termo
+cont = 1
 
-while 
+while cont <= 10:
+    print (f'{termo} -> ', end='')
+    termo += razao
+    cont += 1
+print ('FIM!')
