2c965b9b878bceac2de1c468c323da171e693dab
brunolf23 <138618870+brunolf23@users.noreply.github.com>
2024-03-15 21:12:10 -0300

    exe 70 e 71

--- curso-em-video/modulo 2/exe070.py
+++ curso-em-video/modulo 2/exe070.py
@@ -1,20 +1,27 @@
 # Estatísticas em produtos
 
-total_gasto = produto_caro = 0
+total_gasto = produto_caro = menor = contador = 0
+barato = ''
 
 while True:
     nome = str(input('Digite o nome do produto: '))
-    preco = float(input('Digite o valor do produto: '))
+    preco = float(input('Digite o valor do produto: R$ '))
+    contador += 1
     opcao = ' '
     total_gasto += preco
     
     if preco > 1000:
         produto_caro += 1
     
-    # adicionar o nome do produto mais barato
+    if contador == 1 or preco < menor:
+        menor = preco
+        barato = nome
 
     while opcao not in 'SN':
         opcao = str(input('Continuar comprando? [S/N] ')).strip().upper()[0]
     if opcao == 'N':
         break
-print (f'Finalizado {produto_caro}\n')
+
+print('FIM DO PROGRAMA')
+print(f'O total da compra foi R$: {total_gasto}')
+print(f'O produto mais barato foi {barato} que custa R$: {menor:.2f}')
